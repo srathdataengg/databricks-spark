75feb37d6fdaeecca6a526eb3bdeb6eb980fd7d3
srath.dataengineer@gmail.com <srath.dataengineer@gmail.com>
2024-08-13 20:52:29 +0000

    commit

--- Pyspark-datastructure/StructType.py
+++ Pyspark-datastructure/StructType.py
@@ -6,3 +6,27 @@ schema = ["id","name"]
 df = spark.createDataFrame(data,schema)
 
 df.show()
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+data = [{'id':1,'name':'Soumyakanta'},{'id':2,'name':'soumyakanta'}]
+df = spark.createDataFrame(data)
+df.show(truncate=False)
+df.printSchema()
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructField,StructType,IntegerType,StringType
+
+data = [{'id':1,'name':'sachivji'},{'id':2,'name':'pradhanji'}]
+schema = StructType([
+    StructField(name='id',dataType=IntegerType()),
+    StructField(name='name',dataType=StringType())
+])
+
+df = spark.createDataFrame(data= data,schema=schema)
+
+display(df)
+
